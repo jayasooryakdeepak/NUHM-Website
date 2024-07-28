6e0fd8851f3f594880883fc0b359b2282bacce4b
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-07-28 15:48:33 +0530

    Login Connected to DB

---
+++ login/login_values.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+include('../mysql_conn.php');
+
+global $user_name;
+global $pwd;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//DATA FROM LOGIN PAGE
+$user_name=$_POST['user_name'];
+$pwd=$_POST['pwd'];
+  //$enc_pwd=md5($pwd);
+
+//check connection
+if($conn->connect_error)
+{
+  die("connect failed" . $conn->connect_error);
+}
+
+$query= "SELECT * FROM User_Credentials where user_name='$user_name' AND pwd='$pwd'";
+
+  $res_array = mysqli_query($conn,$query);
+
+  $num = mysqli_num_rows($res_array);
+  $row = mysqli_fetch_assoc($res_array);
+
+  if($num > 0) {
+
+    if($row['user_type'] == 'Admin')
+    {
+      
+      // $_SESSION['loggedin'] = true;
+      // $_SESSION['username'] = $user_name; 
+      // $_SESSION['admin_id'] = $row['id'];
+      // $row = mysqli_fetch_array($res_array);
+      
+      header("Location:../courses.html");
+    }
+    elseif ($row['user_type'] == 'Customer') {
+      $_SESSION['loggedin'] = true;
+      $_SESSION['username'] = $user_name; 
+      $_SESSION['user_id'] = $row['id'];
+      $row = mysqli_fetch_array($res_array);
+      header("Location:it_shop.php");
+    }
+    
+  }
+  else
+  {
+    header("Location:../index-1.html");
+  }
+
+$conn->close();
+?>
