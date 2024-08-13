0b93391cea0a1730c5e97d460f4379aa2cd6f8b4
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-08-13 13:07:38 +0530

    User Details Shown in users page

--- admin/admin_users.php
+++ admin/admin_users.php
@@ -1,17 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Artec_Computers";
-
-global $user_name;
-global $pwd;
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-session_start();
+include('../mysql_conn.php');
 
 $admin_id = $_SESSION['admin_id'];
 
@@ -33,7 +22,7 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>dashboard</title>
+   <title>USERS</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -52,18 +41,16 @@ if(isset($_GET['delete'])){
 
    <div class="box-container">
       <?php
-         $select_users = mysqli_query($conn, "SELECT * FROM `Customer_details`") or die('query failed');
-         $select_users_type = mysqli_query($conn, "SELECT user_type FROM `User_Credentials`") or die('query failed');
+         $select_users = mysqli_query($conn, "SELECT * FROM `User_Credentials`") or die('query failed');
          
          if(mysqli_num_rows($select_users) > 0){
             while($fetch_users = mysqli_fetch_assoc($select_users)){
-               $fetch_users_type = mysqli_fetch_assoc($select_users_type);
       ?>
       <div class="box">
-         <p>user id : <span><?php echo $fetch_users['id']; ?></span></p>
-         <p>username : <span><?php echo $fetch_users['Cust_Name']; ?></span></p>
-         <p>email : <span><?php echo $fetch_users['Email']; ?></span></p>
-         <p>user type : <span style="color:<?php if($fetch_users_type['user_type'] == 'admin'){ echo 'var(--orange)'; }; ?>"><?php echo $fetch_users_type['user_type']; ?></span></p>
+         <p>User Id : <span><?php echo $fetch_users['id']; ?></span></p>
+         <p>User Name : <span><?php echo $fetch_users['user_name']; ?></span></p>
+         <p>Password : <span><?php echo $fetch_users['pwd']; ?></span></p>
+         <p>User type : <span style="color:<?php if($fetch_users['user_type'] == 'admin'){ echo 'var(--orange)'; }; ?>"><?php echo $fetch_users['user_type']; ?></span></p>
          <!-- <a href="admin_users.php?delete=<?php echo $fetch_users_type['id']; ?>" onclick="return confirm('delete this user?');" class="delete-btn">delete</a> -->
       </div>
       <?php
