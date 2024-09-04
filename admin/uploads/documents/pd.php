f7ee363c5b181dc91e9b2ad894e27fae5d57630d
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-09-04 17:04:42 +0530

    Unwanted files removed

--- admin/uploads/documents/pd.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$filename = "Baby_Sankaranarayanan.pdf"; 
-// Header 
-header("Content-type: application/pdf"); header("Content-Length: " . filesize($filename)); 
-// Send the file to the browser. 
-readfile($filename); 
-?>
