a36532626985da8826af13297b8d8f42acb08803
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-09-04 21:28:10 +0530

    New Logo class changed to img-fluid for responsiveness

--- header.php
+++ header.php
@@ -1,82 +1,49 @@
 <div class="bg-top navbar-light">
-      <div class="container">
-        <div
-          class="row no-gutters d-flex align-items-center align-items-stretch"
-        >
-          <div class="col-md-4 d-flex align-items-center py-4">
-            <a href="index.html">
-            <img
-              src="images/NUHM_Logo_Text.png"
-              alt="Your Logo"
-              style="width: 170%; height: auto"
-            />
-          </a>
-            <!-- <a class="navbar-brand" href="index.html"
-              >NUHM <span>Ernakulam</span></a
-            > -->
-          </div>
+  <div class="container">
+    <div class="row no-gutters d-flex align-items-center align-items-stretch">
+      <div class="col-md-4 d-flex align-items-center py-4">
+        <a href="index.html">
+          <img src="images/NUHM_Logo_Text.png" alt="Your Logo" class="img-fluid logo" />
+        </a>
       </div>
     </div>
-    <nav
-      class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light"
-      id="ftco-navbar"
-    >
-      <div class="container d-flex align-items-center px-4">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#ftco-nav"
-          aria-controls="ftco-nav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="oi oi-menu"></span> Menu
-        </button>
-        <!-- <form action="#" class="searchform order-lg-last">
-          <div class="form-group d-flex">
-            <input type="text" class="form-control pl-3" placeholder="Search" />
-            <button type="submit" placeholder class="form-control search">
-              <span class="ion-ios-search"></span>
-            </button>
-          </div>
-        </form> -->
-        <div class="collapse navbar-collapse" id="ftco-nav">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a href="index.html" class="nav-link pl-0">Home</a>
-            </li>
-            <li class="nav-item">
-              <a href="about.html" class="nav-link">About</a>
-            </li>
-
-            <li class="nav-item dropdown">
-              <a
-                href="courses.html"
-                class="nav-link pl-0 dropdown-toggle"
-                data-toggle="dropdown"
-                >Institutions</a
-              >
-              <ul class="dropdown-menu">
-                <li><a href="courses.html" class="dropdown-item">All</a></li>
-                <li><a href="#" class="dropdown-item">UCHC</a></li>
-                <li><a href="#" class="dropdown-item">UPHC's</a></li>
-                <li><a href="#" class="dropdown-item">UHWC's</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="teacher.html" class="nav-link">Services</a>
-            </li>
-            <li class="nav-item">
-              <a href="blog.html" class="nav-link">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a href="downloads.html" class="nav-link">Downloads</a>
-            </li>
-            <li class="nav-item">
-              <a href="contact.html" class="nav-link">Contact</a>
-            </li>
-          </ul>
-        </div>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container d-flex align-items-center px-4">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a href="index.html" class="nav-link pl-0">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="about.html" class="nav-link">About</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="courses.html" class="nav-link pl-0 dropdown-toggle" data-toggle="dropdown">Institutions</a>
+            <ul class="dropdown-menu">
+              <li><a href="courses.html" class="dropdown-item">All</a></li>
+              <li><a href="#" class="dropdown-item">UCHC</a></li>
+              <li><a href="#" class="dropdown-item">UPHC's</a></li>
+              <li><a href="#" class="dropdown-item">UHWC's</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="teacher.html" class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href="blog.html" class="nav-link">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a href="downloads.html" class="nav-link">Downloads</a>
+          </li>
+          <li class="nav-item">
+            <a href="contact.html" class="nav-link">Contact</a>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
+</div>
